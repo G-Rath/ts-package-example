821af4137a9a6063e5161cc4ed031ad04388c80d
Gareth Jones <jones258@gmail.com>
2019-04-08 15:31:57 +1200

    Adjust config

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,75 @@
 import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
 import resolve from 'rollup-plugin-node-resolve';
+import pkg from './package.json';
 
-// /** @type {Array<OutputOptions>}*/
-// const output = [
-//   {
-//     file: 'bundle.cjs.js',
-//     format: 'cjs'
-//   },
-//   {
-//     file: 'bundle.es.js',
-//     format: 'es'
-//   },
-//   {
-//     file: 'bundle.esm.js',
-//     format: 'esm'
-//   }
-// ];
+const typescriptConfig = {
+  cacheRoot: 'tmp/.rpt2_cache',
+  typescript: require('ttypescript'),
+  tsconfig: './tsconfig.build.json'
+};
+const noDeclarationConfig = Object.assign({}, typescriptConfig, {
+  tsconfigOverride: { compilerOptions: { declaration: false } }
+});
 
-/** @type {RollupWatchOptions} */
-const config = {
-  // input: 'src/**/*.ts',
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs'
-    },
-    {
-      file: pkg.module,
-      format: 'es'
-    }
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    'events' // comes from node
-    // ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [
-    resolve(),
-    // multiEntry(),
-    typescript({
-      clean: true,
-      typescript: require('ttypescript'),
-      include: ['**/*.ts'],
-      tsconfig: './tsconfig.build.json'
-    })
-  ]
+const makeExternalPredicate = externalArr => {
+  if (externalArr.length === 0) {
+    return () => false;
+  }
+  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`);
+  return id => pattern.test(id);
 };
 
-export default config;
+const deps = Object.keys(pkg.dependencies || {});
+const peerDeps = Object.keys(pkg.peerDependencies || {});
+
+const config = {
+  input: 'src/index.ts',
+  external: makeExternalPredicate(deps.concat(peerDeps))
+};
+
+const umd = Object.assign({}, config, {
+  output: {
+    file: 'dist/ts-package-example.js',
+    format: 'umd',
+    name: 'ts-package-example',
+    exports: 'named',
+    globals: {
+      'style-value-types': 'valueTypes'
+    }
+  },
+  external: makeExternalPredicate(peerDeps),
+  plugins: [
+    resolve(),
+    typescript(noDeclarationConfig)
+  ]
+});
+
+const umdProd = Object.assign({}, umd, {
+  output: Object.assign({}, umd.output, {
+    file: pkg.unpkg
+  }),
+  plugins: [
+    resolve(),
+    typescript(noDeclarationConfig)
+  ]
+});
+
+const es = Object.assign({}, config, {
+  output: {
+    file: pkg.module,
+    format: 'es',
+    exports: 'named'
+  },
+  plugins: [resolve(), typescript(noDeclarationConfig)]
+});
+
+const cjs = Object.assign({}, config, {
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+    exports: 'named'
+  },
+  plugins: [resolve(), typescript(typescriptConfig)]
+});
+
+export default [umd, umdProd, es, cjs];
